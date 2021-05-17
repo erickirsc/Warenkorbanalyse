08c09408a9f9c9decf447b4b8eb6c1c7059bfaa9
Falk Marinov <59347986+falkmarinov@users.noreply.github.com>
2021-05-17 22:32:19 +0200

    Initial commit

---
+++ src/main/java/hsel/softsmart/warenkorbanalyse/WarenkorbanalyseApplication.java
@@ -0,0 +1,13 @@
+package hsel.softsmart.warenkorbanalyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenkorbanalyseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarenkorbanalyseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hsel/softsmart/warenkorbanalyse/WarenkorbanalyseApplicationTests.java
@@ -0,0 +1,13 @@
+package hsel.softsmart.warenkorbanalyse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarenkorbanalyseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
