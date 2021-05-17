7d9444ba1bca02cf9f661d871f6c620cf2c77d57
Falk Marinov <59347986+falkmarinov@users.noreply.github.com>
2021-05-18 01:44:17 +0200

    Add Controller for each view

---
+++ src/main/java/hsel/softsmart/warenkorbanalyse/controller/MarketingMeasuresController.java
@@ -0,0 +1,15 @@
+package hsel.softsmart.warenkorbanalyse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("marketing-measures")
+public class MarketingMeasuresController {
+
+    @GetMapping
+    public String index() {
+        return "marketingMeasures";
+    }
+}
---
+++ src/main/java/hsel/softsmart/warenkorbanalyse/controller/SettingsController.java
@@ -0,0 +1,15 @@
+package hsel.softsmart.warenkorbanalyse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("settings")
+public class SettingsController {
+
+    @GetMapping
+    public String index() {
+        return "settings";
+    }
+}
