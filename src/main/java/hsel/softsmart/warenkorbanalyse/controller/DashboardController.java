fb7e897ce4f3c63b1d4e2cf915a08d487bd93b76
Falk Marinov <59347986+falkmarinov@users.noreply.github.com>
2021-05-18 02:07:23 +0200

    Upload CSV-File

--- src/main/java/hsel/softsmart/warenkorbanalyse/controller/DashboardController.java
+++ src/main/java/hsel/softsmart/warenkorbanalyse/controller/DashboardController.java
@@ -1,8 +1,15 @@
 package hsel.softsmart.warenkorbanalyse.controller;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -11,5 +18,13 @@ public class DashboardController {
     @GetMapping
     public String index() {
         return "dashboard";
+    }
+
+    @SneakyThrows
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file, Model model) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        //TODO: Use reader and parse the file into WEKA API
+        return "redirect:/dashboard";
     }
 }
