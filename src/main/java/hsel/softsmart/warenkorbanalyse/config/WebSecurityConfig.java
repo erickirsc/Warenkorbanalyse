d054885daf7eeb62bbc60da542636cb31bc318de
Falk Marinov <59347986+falkmarinov@users.noreply.github.com>
2021-06-13 16:31:44 +0200

    Update third-party-framework organisation

--- src/main/java/hsel/softsmart/warenkorbanalyse/config/WebSecurityConfig.java
+++ src/main/java/hsel/softsmart/warenkorbanalyse/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/dist/**", "/css/**", "/img/**", "/plugins/**").permitAll()
+                .antMatchers("/img/**", "/scripts/**", "/styles/**", "/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
